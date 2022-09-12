7ba70977b09b2f2a3aa7ed9ee4b860d241b23fe3
Jos O'shea <jos@joshea.dev>
2022-09-12 01:46:40 -0400

    update to cors

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem "puma", "~> 5.0"
 # gem "jbuilder"
 gem 'http' 
 gem 'rack-cors'
+gem 'twilio-ruby'
 # Use Redis adapter to run Action Cable in production
 # gem "redis", "~> 4.0"
 
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -17,7 +17,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins '*'
+      origins 'joshea.dev:80'
       resource '*', headers: :any, methods: [:get, :post]
     end
   end
