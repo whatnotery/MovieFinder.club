3087fdb326ed9182d8e249464c2d5949464b8374
Jos O'shea <jos@joshea.dev>
2022-09-12 23:51:31 -0400

    best practices

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -3,13 +3,14 @@ class FilmsController < ApplicationController
     require 'twilio-ruby'
 
     def get_latest_film_id
-        response = HTTP.get('https://api.themoviedb.org/3/movie/latest', :params => {:api_key => 'b07d3efad9e75e49c88e831539462c48'})
+        response = HTTP.get('https://api.themoviedb.org/3/movie/latest', :params => {:api_key => ENV['MOVIE_DB_API_KEY']
+            })
         response.parse['id'] if response.status == 200
     end
 
     def get_random_film 
         movie_id = rand(get_latest_film_id);
-        response = HTTP.get("https://api.themoviedb.org/3/movie/#{movie_id}", :params => {:api_key => 'b07d3efad9e75e49c88e831539462c48'})
+        response = HTTP.get("https://api.themoviedb.org/3/movie/#{movie_id}", :params => {:api_key => ENV['MOVIE_DB_API_KEY']})
         data = response.parse if response.status == 200
         if data["adult"] == true || !data['title'] || !data['poster_path'] || !data['overview']
             get_random_film
