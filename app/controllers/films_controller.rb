c0c27e06548f2bd7181245879b28d1a878794afb
Jos O'shea <jos@joshea.dev>
2022-09-13 01:17:24 -0400

    update

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -25,10 +25,11 @@ class FilmsController < ApplicationController
 
     def twilio
         data = get_random_film
-        poster = "https://image.tmdb.org/t/p/w300/#{data['poster_path']}"
+        poster = "https://image.tmdb.org/t/p/w300/'#{data['poster_path']}"
+        imdb = "https://www.imdb.com/title/#{data['imdb_id']}"
         twiml = Twilio::TwiML::MessagingResponse.new do |r|
-            r.message body: "#{data['original_title']} \n -------- \n #{data['overview']}", media_url: [poster]
-
+            r.message body: "#{data['original_title']} (#{data['release_date'].slice(0, 4)}) \n -------- \n #{data['overview']}", media_url: poster
+            r.message body: imdb
           end
         render xml: twiml
     end
