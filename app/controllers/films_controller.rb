434cce9a5c6b8fd17b8f2e57496d34f8b2c0285e
Jos O'shea <jos@joshea.dev>
2022-09-15 18:45:52 -0400

    change post request response

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -24,12 +24,14 @@ class FilmsController < ApplicationController
     end
 
     def twilio
-        data = get_random_film
-        poster = "https://image.tmdb.org/t/p/w300/'#{data['poster_path']}"
-        imdb = "https://www.imdb.com/title/#{data['imdb_id']}"
+        incoming = request.body.read
+        #data = get_random_film
+        #poster = "https://image.tmdb.org/t/p/w300/'#{data['poster_path']}"
+        #imdb = "https://www.imdb.com/title/#{data['imdb_id']}"
         twiml = Twilio::TwiML::MessagingResponse.new do |r|
-            r.message body: "#{data['original_title']} (#{data['release_date'].slice(0, 4)}) \n -------- \n #{data['overview']}", media_url: poster
-            r.message body: imdb
+           # r.message body: "#{data['original_title']} (#{data['release_date'].slice(0, 4)}) \n -------- \n #{data['overview']}", media_url: poster
+           # r.message body: imdb
+           r.message body: "#{incoming}"
           end
         render xml: twiml
     end
