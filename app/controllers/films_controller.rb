91a294db18e920b4f882358d0fa4ae357f9ab8b5
Jos O'shea <jos@joshea.dev>
2022-10-20 19:12:13 -0400

    refactor

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -1,30 +1,12 @@
 class FilmsController < ApplicationController
-    require 'http'
-    require 'twilio-ruby'
-
-    def get_latest_film_id
-        response = HTTP.get('https://api.themoviedb.org/3/movie/latest', :params => {:api_key => ENV['MOVIE_DB_API_KEY']
-            })
-        response.parse['id'] if response.status == 200
-    end
-
-    def get_random_film 
-        movie_id = rand(get_latest_film_id);
-        response = HTTP.get("https://api.themoviedb.org/3/movie/#{movie_id}", :params => {:api_key => ENV['MOVIE_DB_API_KEY']})
-        data = response.parse if response.status == 200
-        if data["adult"] == true || !data['title'] || !data['poster_path'] || !data['overview']
-            get_random_film
-        else
-            data
-        end
-    end
-
+    
+    
     def index
-       render json: get_random_film
+       render json: Film.get_random_film
     end
 
     def twilio
-        data = get_random_film
+        data = Film.get_random_film
         poster = "https://image.tmdb.org/t/p/w300/'#{data['poster_path']}"
         imdb = "https://www.imdb.com/title/#{data['imdb_id']}"
         twiml = Twilio::TwiML::MessagingResponse.new do |r|
--- app/models/film.rb
+++ app/models/film.rb
@@ -1,3 +1,21 @@
 class Film < ApplicationRecord
-  
+    require 'http'
+
+    def self.get_latest_film_id
+        response = HTTP.get('https://api.themoviedb.org/3/movie/latest', :params => {:api_key => ENV['MOVIE_DB_API_KEY']
+            })
+        response.parse['id'] if response.status == 200
+    end
+
+    def self.get_random_film 
+        movie_id = rand(Film.get_latest_film_id);
+        response = HTTP.get("https://api.themoviedb.org/3/movie/#{movie_id}", :params => {:api_key => ENV['MOVIE_DB_API_KEY']})
+        data = response.parse if response.status == 200
+        if data["adult"] == true || !data['title'] || !data['poster_path'] || !data['overview']
+            Film.get_random_film
+        else
+            data
+        end
+    end
+
 end
