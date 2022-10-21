8608301c056d27249189e830631b967b73a3a955
Jos O'shea <jos@joshea.dev>
2022-10-21 19:11:22 -0400

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-    root "films#index"
-
     get '/films' => "films#index"
     post '/films' => 'films#twilio'
 
