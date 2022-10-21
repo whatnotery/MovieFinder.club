e122982cdfb49b18e0b07cf5a2fa7a9323ee9468
Jos O'shea <jos@joshea.dev>
2022-10-21 18:45:58 -0400

    updates

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -2,18 +2,19 @@ class FilmsController < ApplicationController
     require 'twilio-ruby'
 
     def index
-       render json: Film.get_random_film("#{params['genre']}")
+        if params['genre'].present?
+            render json: Film.get_random_film(params['genre'])
+        else
+            render json: Film.get_random_film()
+        end
     end
 
     def twilio
-        data = Film.get_random_film("#{params['body']}")
-        poster = "https://image.tmdb.org/t/p/w300/'#{data['poster_path']}"
-        imdb = "https://www.imdb.com/title/#{data['imdb_id']}"
-        twiml = Twilio::TwiML::MessagingResponse.new do |r|
-            r.message body: "#{data['title']} (#{data['release_date'].slice(0, 4)}) \n -------- \n #{data['overview']}", media_url: poster
-            r.message body: imdb
-          end
-        render xml: twiml
+        if params['genre'].present?
+            render xml: twiml(params['genre'])
+        else
+            render xml: twiml()
+        end
     end
 
 end
--- app/models/film.rb
+++ app/models/film.rb
@@ -12,13 +12,28 @@ class Film < ApplicationRecord
         response = HTTP.get("https://api.themoviedb.org/3/movie/#{movie_id}", :params => {:api_key => ENV['MOVIE_DB_API_KEY']})
         data = response.parse if response.status == 200
         if !genre.empty?
-            if !data['genres'].empty? and data['genres'][0]['name'] == genre[0].capitalize and data["adult"] == false or !data['title'].blank? or !data['poster_path'].blank? or !data['overview'].blank? or !data['tagline'].blank?  
+            if !data['genres'].empty? and data['genres'][0]['name'] == genre[0].capitalize and data["adult"] == false or !data['title'].blank? or !data['poster_path'].blank? or !data['overview'].nil? or !data['tagline'].nil?  
                 data
             else
                 get_random_film(genre[0].capitalize) 
             end
         else
             get_random_film()
+        end
+    end
+
+    def twiml(*params)
+        if params.empty?
+            data = Film.get_random_film()
+        else
+            data = Film.get_random_film(params)
+        end
+        poster = "https://image.tmdb.org/t/p/w300/'#{data['poster_path']}"
+        imdb = "https://www.imdb.com/title/#{data['imdb_id']}"
+        twiml = Twilio::TwiML::MessagingResponse.new do |r|
+            r.message body: media_url: poster
+            r.message body: "#{data['title']} (#{data['release_date'].slice(0, 4)}) \n -------- \n #{data['overview']}"
+            r.message body: imdb
         end
     end
 end
