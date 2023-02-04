6bf45d721cf7f55ef0e58ffa7841488ef4ba4b2d
Jos O'shea <jos@joshea.dev>
2023-02-03 19:40:06 -0500

    Update film.rb

--- app/models/film.rb
+++ app/models/film.rb
@@ -20,7 +20,7 @@ class Film < ApplicationRecord
 
     def self.twiml
         data = Film.get_random_film
-        poster = "https://image.tmdb.org/t/p/w300/'#{data['poster_path']}"
+        poster = "https://image.tmdb.org/t/p/w300'#{data['poster_path']}"
         imdb = "https://www.imdb.com/title/#{data['imdb_id']}"
         twiml = Twilio::TwiML::MessagingResponse.new do |r|
             r.message body: "#{data['original_title']} (#{data['release_date'].slice(0, 4)}) \n -------- \n #{data['overview']}", media_url: poster
